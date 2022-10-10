4a492d1711bc7b322d52c7641699edbbc5893f0d
LuvBoy-76 <78361904+cjboy76@users.noreply.github.com>
2022-10-10 19:01:44 +0800

    :+1:  update: update README.md

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import { ref } from 'vue';
 import VueEcailr from './components/VueEcailr.vue';
 
@@ -11,25 +9,10 @@ function toggle() {
 </script>
 
 <template>
-  <header style="display: flex">
-    <div>this is icon</div>
+  <header>
     <vue-ecailr :stop="stop">
-      <h1 style="display: flex">hello world 這是棒球卡片</h1>
+      <h1>👋 👋 Hello VueEcailr</h1>
     </vue-ecailr>
   </header>
   <button @click="toggle">toggle</button>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
