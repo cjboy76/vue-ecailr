428c3d5e61db0c42007b51aa531694ff8da831c7
LuvBoy-76 <78361904+cjboy76@users.noreply.github.com>
2022-10-12 01:10:55 +0800

    :pencil: update: demo page's style to tailwind

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,43 @@
 import { ref } from 'vue';
 import VueEcailr from './components/VueEcailr.vue';
 import GitHubIcon from './components/GitHubIcon.vue';
+import CheckIcon from './components/CheckIcon.vue';
 
 const stop = ref(false);
+const installCommand = ref('npm install vue-ecailr');
+const copied = ref(false);
 function toggle() {
   stop.value = !stop.value;
+}
+function copyToClipBoard() {
+  navigator.clipboard.writeText(installCommand.value).then(() => {
+    copied.value = true;
+  });
 }
 </script>
 
 <template>
-  <div class="content">
+  <div class="m-4">
     <vue-ecailr :stop="stop">
       <h1>👋 👋 Hello VueEcailr</h1>
     </vue-ecailr>
   </div>
   <button @click="toggle">Toggle Stop</button>
-  <div class="codeBlock">
-    <div>
-      <code class="code">$ npm install vue-ecailr</code>
-    </div>
+  <div class="mt-16 mb-4">
+    <code class="inline-block w-[400px] bg-black py-2 px-4 my-4 mx-6 rounded">
+      <div class="flex justify-between items-center">
+        <span> $ {{ installCommand }} </span>
+        <button
+          class="rounded py-2 px-4 text-sm text-bold bg-black cursor-pointer text-white"
+          @click="copyToClipBoard"
+        >
+          <span v-show="!copied"> Copy </span>
+          <span v-show="copied" class="flex items-center">
+            Copied <CheckIcon class="ml-2" />
+          </span>
+        </button>
+      </div>
+    </code>
   </div>
   <div>
     <a href="https://github.com/cjboy76/vue-ecailr" target="_blank">
@@ -29,18 +48,3 @@ function toggle() {
     </a>
   </div>
 </template>
-
-<style>
-.content {
-  margin: 1rem;
-}
-.codeBlock {
-  margin: 4rem 0 2rem;
-}
-.codeBlock .code {
-  background: #000;
-  padding: 1rem 1.5rem;
-  margin: 1rem 1.5rem;
-  border-radius: 5px;
-}
-</style>
