d9e9db75f3566dfaaff2460d46dd7c238dd3cc9f
LuvBoy-76 <78361904+cjboy76@users.noreply.github.com>
2022-10-12 22:23:43 +0800

    :skull: fix: wrong ci command

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import VueEcailr from './components/VueEcailr.vue';
+import VueEcailr from 'vue-ecailr';
 import GitHubIcon from './components/GitHubIcon.vue';
 import CheckIcon from './components/CheckIcon.vue';
 
